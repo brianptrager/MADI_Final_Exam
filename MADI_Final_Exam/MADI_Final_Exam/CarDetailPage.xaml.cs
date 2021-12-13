ae42ecd7e19a6863dee8f8d69370d3b487bf3270
Brian Trager <brian.p.trager@outlook.com>
2021-12-12 19:35:41 -0500

    Add project files.

---
+++ MADI_Final_Exam/MADI_Final_Exam/CarDetailPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MADI_Final_Exam
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarDetailPage : ContentPage
+    {
+        public CarDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MADI_Final_Exam/MADI_Final_Exam/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MADI_Final_Exam.Models
+{
+    class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public int Miles { get; set; }
+        public string ExteriorColor { get; set; }
+        public string InteriorColor { get; set; }
+        public string Image { get; set; }
+    }
+}
